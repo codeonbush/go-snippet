5b871da90d8b807527405e3fb8ee6cfef492b27d
PunkCoder <codeonbush@foxmail.com>
2023-02-21 11:47:00 +0800

    fix

--- slice/slice.go
+++ slice/slice.go
@@ -1,101 +1,140 @@
 package main
 
-import "fmt"
-
-var arr = [...]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-var slice0 []int = arr[2:8]
-var slice1 []int = arr[0:6]        //可以简写为 var slice []int = arr[:end]
-var slice2 []int = arr[5:10]       //可以简写为 var slice[]int = arr[start:]
-var slice3 []int = arr[0:len(arr)] //var slice []int = arr[:]
-var slice4 = arr[:len(arr)-1]      //去掉切片的最后一个元素
+import (
+	"fmt"
+	"math/rand"
+	"runtime"
+	"time"
+)
 
 func main() {
-	////前包后不包（下标）
-	//fmt.Printf("全局变量：arr %v\n", arr)
-	//fmt.Printf("全局变量：slice0 %v\n", slice0)
-	//fmt.Printf("全局变量：slice1 %v\n", slice1)
-	//fmt.Printf("全局变量：slice2 %v\n", slice2)
-	//fmt.Printf("全局变量：slice3 %v\n", slice3)
-	//fmt.Printf("全局变量：slice4 %v\n", slice4)
-	//fmt.Printf("-----------------------------------\n")
-	//arr2 := [...]int{9, 8, 7, 6, 5, 4, 3, 2, 1, 0}
-	//slice5 := arr[2:8]
-	//slice6 := arr[0:6]         //可以简写为 slice := arr[:end]
-	//slice7 := arr[5:10]        //可以简写为 slice := arr[start:]
-	//slice8 := arr[0:len(arr)]  //slice := arr[:]
-	//slice9 := arr[:len(arr)-1] //去掉切片的最后一个元素
-	//fmt.Printf("局部变量： arr2 %v\n", arr2)
-	//fmt.Printf("局部变量： slice5 %v\n", slice5)
-	//fmt.Printf("局部变量： slice6 %v\n", slice6)
-	//fmt.Printf("局部变量： slice7 %v\n", slice7)
-	//fmt.Printf("局部变量： slice8 %v\n", slice8)
-	//fmt.Printf("局部变量： slice9 %v\n", slice9)
-	//
-	////未初始化的slice为nil
-	//var s1 []int
-	//if s1 == nil {
-	//	fmt.Println("是空")
-	//} else {
-	//	fmt.Println("不是空")
-	//}
-	//s2 := []int{}
-	//// 3.make()
-	//var s3 []int = make([]int, 0)
-	//fmt.Println(s1 == nil, s2 == nil, s3 == nil)
-	//fmt.Println(s1, s2, s3)
-	//
-	//// 4.初始化赋值
-	//var s4 []int = make([]int, 0, 0)
-	//fmt.Println(s4)
-	//s5 := []int{1, 2, 3}
-	//fmt.Println(s5)
-	//
-	//// 读写操作实际目标是底层数组，只需注意索引号的差别。
-	//data := [...]int{0, 1, 2, 3, 4, 5}
-	//s := data[2:4]
-	//s[0] += 100
-	//s[1] += 200
-	//fmt.Println(s)
-	//fmt.Println(data)
-	//
-	//// 使用 make 动态创建slice，避免了数组必须用常量做长度的麻烦。还可用指针直接访问底层数组，退化成普通数组操作。
-	//ss := []int{0, 1, 2, 3}
-	//p := &ss[2] // *int, 获取底层数组元素指针。
-	//*p += 100
-	//fmt.Println(ss)
+	value()
 
+	checkNil()
 
-	//向 slice 尾部添加数据，返回新的 slice 对象。
-	//s1 := make([]int, 0, 5)
-	//fmt.Printf("%p\n", &s1)
-	//
-	//s2 := append(s1, 1)
-	//fmt.Printf("%p\n", &s2)
-	//fmt.Println(s1, s2)
+	arrAndSlice()
 
-	//切片拷贝，copy ：函数 copy 在两个 slice 间复制数据，复制长度以 len 小的为准。两个 slice 可指向同一底层数组，允许元素区间重叠。
-	//s1 := []int{1, 2, 3, 4, 5}
-	//fmt.Printf("slice s1 : %v\n", s1)
-	//s2 := make([]int, 10)
-	//fmt.Printf("slice s2 : %v\n", s2)
-	//copy(s2, s1)
-	//fmt.Printf("copied slice s1 : %v\n", s1)
-	//fmt.Printf("copied slice s2 : %v\n", s2)
-	//s3 := []int{1, 2, 3}
-	//fmt.Printf("slice s3 : %v\n", s3)
-	//s3 = append(s3, s2...)
-	//fmt.Printf("appended slice s3 : %v\n", s3)
-	//s3 = append(s3, 4, 5, 6)
-	//fmt.Printf("last slice s3 : %v\n", s3)
+	copySlice()
 
-	//切片resize
-	var a = []int{1, 3, 4, 5}
-	fmt.Printf("slice a : %v , len(a) : %v\n, cap(a) : %v\n", a, len(a), cap(a))
-	b := a[1:2]
-	fmt.Printf("slice b : %v , len(b) : %v\n, cap(b) : %v\n", b, len(b), cap(b))
-	c := b[0:3] // 截取
-	fmt.Printf("slice c : %v , len(c) : %v\n, cap(c) : %v\n", c, len(c), cap(c))
-	fmt.Println(b[1])
+	testLastChars(lastNumsBySlice)
+	testLastChars(lastNumsBySlice)
 }
 
+//切片下标取值，下标前闭后开
+func value() {
+	var arr = [...]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	var slice0 = arr[2:8]
+	var slice1 = arr[0:6]         //可以简写为 var slice []int = arr[:end]
+	var slice2 = arr[5:10]        //可以简写为 var slice[]int = arr[start:]
+	var slice3 = arr[0:len(arr)]  //var slice []int = arr[:]
+	var slice4 = arr[:len(arr)-1] //去掉切片的最后一个元素
+	fmt.Printf("全局变量：arr %v\n", arr)
+	fmt.Printf("全局变量：slice0 %v\n", slice0)
+	fmt.Printf("全局变量：slice1 %v\n", slice1)
+	fmt.Printf("全局变量：slice2 %v\n", slice2)
+	fmt.Printf("全局变量：slice3 %v\n", slice3)
+	fmt.Printf("全局变量：slice4 %v\n", slice4)
+	fmt.Printf("-----------------------------------\n")
+	return
+}
 
+//2. 未初始化的slice为nil
+func checkNil() {
+	var s1 []int
+	s2 := []int{}
+	s3 := make([]int, 0)
+	fmt.Println(s1 == nil, s2 == nil, s3 == nil)
+	fmt.Println(s1, s2, s3)
+	fmt.Printf("-----------------------------------\n")
+	return
+}
+
+func arrAndSlice() {
+	// 读写操作实际目标是底层数组，只需注意索引号的差别。
+	data := [...]int{0, 1, 2, 3, 4, 5}
+	s := data[2:4]
+	s[0] += 100
+	s[1] += 200
+	fmt.Println(s)
+	fmt.Println(data) //底层数组data也会改变
+
+	c := data[:0]
+	c = append(c, 0) //长度为0的切片添加元素时，也会影响底层数组
+	fmt.Println(c[0], data)
+
+	// 使用 make 动态创建slice，避免了数组必须用常量做长度的麻烦。还可用指针直接访问底层数组，退化成普通数组操作。
+	ss := []int{0, 1, 2, 3}
+	p := &ss[2] // *int, 获取底层数组元素指针。
+	*p += 100
+	fmt.Println(ss)
+
+	//向 slice 尾部添加数据，返回新的 slice 对象。
+	s1 := make([]int, 510, 510)
+	fmt.Printf("%p\n", &s1)
+	s2 := append(s1, 1)
+	fmt.Printf("%p\n", &s2)
+	fmt.Println(s1, s2)
+	fmt.Println(cap(s1), len(s1)) //扩容时，小数组（len256以下）翻倍，大数组1.25倍
+	fmt.Println(cap(s2), len(s2))
+
+	//切片resize
+	var a = []int{0, 1, 2, 3, 4, 5, 6}
+	fmt.Printf("slice a : %v , len(a) : %v, cap(a) : %v\n", a, len(a), cap(a))
+	b := a[1:4]
+	fmt.Printf("slice b : %v , len(b) : %v, cap(b) : %v\n", b, len(b), cap(b))
+	d := b[0:5] //截取时考虑底层数组是否越界，如果通过索引直接取值则考虑切片是否越界
+	fmt.Printf("slice c : %v , len(c) : %v, cap(c) : %v\n", d, len(d), cap(d))
+	return
+}
+
+func copySlice() {
+	//切片拷贝，copy ：函数 copy 在两个 slice 间复制数据，复制长度以 len 小的为准。两个 slice 可指向同一底层数组，允许元素区间重叠。
+	s1 := []int{1, 2, 3, 4, 5}
+	fmt.Printf("slice s1 : %v\n", s1)
+	s2 := make([]int, 10)
+	fmt.Printf("slice s2 : %v\n", s2)
+	copy(s2, s1)
+	fmt.Printf("copied slice s1 : %v\n", s1)
+	fmt.Printf("copied slice s2 : %v\n", s2)
+	s3 := []int{1, 2, 3}
+	fmt.Printf("slice s3 : %v\n", s3)
+	s3 = append(s3, s2...)
+	fmt.Printf("appended slice s3 : %v\n", s3)
+	s3 = append(s3, 4, 5, 6)
+	fmt.Printf("last slice s3 : %v\n", s3)
+}
+
+//原切片基础上进行切片
+func lastNumsBySlice(origin []int) []int {
+	return origin[len(origin)-2:]
+}
+
+//创建了一个新的切片，将 origin 的最后两个元素拷贝到新切片上，然后返回新切片
+func lastNumsByCopy(origin []int) []int {
+	result := make([]int, 2)
+	copy(result, origin[len(origin)-2:])
+	return result
+}
+
+//随机生成 n 个 int 整数，64位机器上，一个 int 占 8 Byte，128 * 1024 个整数恰好占据 1 MB 的空间
+func generateWithCap(n int) []int {
+	rand.Seed(time.Now().UnixNano())
+	nums := make([]int, 0, n)
+	for i := 0; i < n; i++ {
+		nums = append(nums, rand.Int())
+	}
+	return nums
+}
+
+func testLastChars(f func([]int) []int) {
+	ans := make([][]int, 0)
+	for k := 0; k < 100; k++ {
+		origin := generateWithCap(128 * 1024) // 1M
+		ans = append(ans, f(origin))
+	}
+	_ = ans
+	var rtm runtime.MemStats
+	runtime.ReadMemStats(&rtm)
+	fmt.Println("%.2f MB", float64(rtm.Alloc)/1024./1024.)
+	return
+}
