421cbf2b4b3d8be16234891090acd1f6f5ff3840
PunkCoder <codeonbush@foxmail.com>
2023-02-22 15:29:18 +0800

    fix

--- channel/channel.go
+++ channel/channel.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"sync"
+	"time"
 )
 
 func main() {
@@ -13,7 +15,6 @@ func main() {
 	////多个协程成功输出
 	//go func() {c <- 1}()              // 此时c会被阻塞 直到值被取走前都不可在塞入新值
 	//go func() {println(<-c)}()
-
 
 	// 带缓存的chan在单个协程中不会阻塞,超过缓冲区大小阻塞
 	//c := make(chan int, 1)
@@ -54,9 +55,10 @@ func main() {
 	//MySelect(make(chan int), make(chan int))
 
 	// 接收动作发生在发送动作之前
-	out := make(chan int)
-	out <- 2
-	go f1(out)
+	//out := make(chan int)
+	//out <- 2
+	//go f1(out)
+	ProductAndConsumer()
 }
 
 // select可以同时轮询多个chan，不会造成阻塞
@@ -72,5 +74,32 @@ func MySelect(ch chan int, ch2 chan int) {
 }
 
 func f1(in chan int) {
+	time.Sleep(time.Second)
 	fmt.Println(<-in)
+}
+
+/**生产者消费者的例子*/
+func ProductAndConsumer() {
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	//带有缓冲区的通道
+	cint := make(chan int, 10)
+	go func() {
+		//product  ，循环往通道中写入一个元素
+		for i := 0; i < 100; i++ {
+			cint <- i
+		}
+		//关闭通道
+		close(cint)
+	}()
+	go func() {
+		defer wg.Done()
+		//consumer   遍历通道消费元素并打印
+		for temp := range cint {
+			fmt.Println(temp)
+			//len函数可以查看当前通道元素个数
+			fmt.Println("当前通道元素个数", len(cint))
+		}
+	}()
+	wg.Wait()
 }
